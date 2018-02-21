199c8f3c07241d0ae7064d7c75149c4aed7cfe08
J. Andrew Casey-Clyde <jacaseyclyde@gmail.com>
2018-02-20 22:45:33 -0800

    fixed the import issues. can now work with data in whatever combination

--- main.py
+++ main.py
@@ -10,12 +10,13 @@ Authors:
 
 import numpy as np
 
+
 if __name__ == "__main__":
     # import the data from csv. we can either keep doing a file like this
     # or do the data import in python. current data is just anything
     # useful, we should feel free to change the data we pull based on what
     # we actually use
     ra, dec, u, g, r, i, z, redshift = np.genfromtxt('data.csv', delimiter=',',
-                                                     skip_header=2)
+                                                     skip_header=2).T
     # the rest of this space can be used for actually running everything. do
     # other development either in a different file or above the main function
