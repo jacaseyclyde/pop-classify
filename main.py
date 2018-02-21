f79bd577d2ebce194e6fe8c0d1e784821854c19b
J. Andrew Casey-Clyde <jacaseyclyde@gmail.com>
2018-02-20 18:29:03 -0800

    a first pass at getting the data parsed/setup. working out some bugs

--- main.py
+++ main.py
@@ -9,13 +9,17 @@ Authors:
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
+
+
 
 if __name__ == "__main__":
     # import the data from csv. we can either keep doing a file like this
     # or do the data import in python. current data is just anything
     # useful, we should feel free to change the data we pull based on what
     # we actually use
-    ra, dec, u, g, r, i, z, redshift = np.genfromtxt('data.csv', delimiter=',',
+    [ra, dec, u, g, r, i, z, redshift] = np.genfromtxt('data.csv', delimiter=',',
                                                      skip_header=2)
-    # the rest of this space can be used for actually running everything. do
-    # other development either in a different file or above the main function
+    
+    data = np.array([u-g,g-r,r-i])
+    print(data.shape)
