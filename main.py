5e142a8b4497f264114fbb306767e9c9e4d3aaec
J. Andrew Casey-Clyde <jacaseyclyde@gmail.com>
2018-03-07 22:22:57 -0800

    basic SVM implementation

--- main.py
+++ main.py
@@ -10,9 +10,7 @@ Authors:
 
 import numpy as np
 
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-
+from sklearn import svm
 
 def SVMclassifier(data,classes):
     (m, n) = data.shape  # dimensionality and number of points, respectively
@@ -24,7 +22,12 @@ def SVMclassifier(data,classes):
             gram[i, j] = np.dot(data[:, i], data[:, j])
             gram[j, i] = gram[i, j]
             
-    #print(gram)
+    clf = svm.SVC(kernel='precomputed')
+    clf.fit(gram, classes)
+    
+    print(clf.score(gram, classes))
+    
+    return clf
 
 if __name__ == "__main__":
     # Import the data in 2 seperate stmts b/c genfromtxt doesnt like multityping
@@ -39,51 +42,4 @@ if __name__ == "__main__":
     # for now this is just what's in the sample data. this could be automated
     # but I want to keep some control over the colors themselves.
     
-    classes = np.unique(subClass)
-    cdict = {}
-    
-    cdict = {'O':'blue','B':'lightskyblue','A':'white','F':'lightyellow',
-             'G':'yellow','K':'orange','M':'red','T':'brown','L':'saddlebrown',
-             'C':'black','W':'purple'}
-    
-    colClass = []
-    for c in subClass:
-        colClass.append(cdict[c[0]])
-        
-    colClass = np.array(colClass)
-    
-    # plot the classes/colors
-    fig1, ax1 = plt.subplots(3, 3, sharex=True, sharey=True)
-    fig1.set_size_inches(24,24)
-    
-    ax1[0, 0].scatter(colordata[0], colordata[1], c=colClass, s=50)
-    ax1[0, 0].set_ylabel('$g-r$')
-    ax1[0, 0].set_xticklabels([])
-    ax1[0, 0].set_yticklabels([])
-    
-    ax1[0, 1].axis('off')
-    ax1[0, 2].axis('off')
-    
-    ax1[1, 0].scatter(colordata[0], colordata[2], c=colClass, s=50)
-    ax1[1, 0].set_ylabel('$r-i$')
-    
-    ax1[1, 1].scatter(colordata[1], colordata[2], c=colClass, s=50)
-    
-    ax1[1, 2].axis('off')
-    
-    ax1[2, 0].scatter(colordata[0], colordata[3], c=colClass, s=50)
-    ax1[2, 0].set_xlabel('$u-g$')
-    ax1[2, 0].set_ylabel('$i-z$')
-    
-    ax1[2, 1].scatter(colordata[1], colordata[3], c=colClass, s=50)
-    ax1[2, 1].set_xlabel('$g-r$')
-    
-    ax1[2, 2].scatter(colordata[0], colordata[3], c=colClass, s=50)
-    ax1[2, 2].set_xlabel('$r-i$')
-    
-    fig1.subplots_adjust(hspace=0, wspace=0)
-    fig1.show()
-    fig1.savefig('./out/color_corner.pdf')
-    fig1.savefig('./out/color_corner.png')
-    
-    SVMclassifier(colordata,subClass)
+    svclf = SVMclassifier(colordata,subClass)
