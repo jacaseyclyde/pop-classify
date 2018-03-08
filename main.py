1c4b19c4b0fd4a775892c357ee56037cdbd421e4
J. Andrew Casey-Clyde <jacaseyclyde@gmail.com>
2018-03-07 23:17:07 -0800

    added timing data to svm classifier. caught another error with the plots

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ Authors:
     Alex Colebaugh
     Kevin Prasad
 """
+import time
 
 import numpy as np
 
@@ -44,10 +45,10 @@ def CornerPlot(data,labels):
                 ax1[i, j].axis('off')
                 
             else:
-                ax1[i, j].scatter(colordata[j], colordata[nAx - 1 - i], c=colClass, s=50)
+                ax1[i, j].scatter(colordata[j], colordata[i + 1], c=colClass, s=50)
                 
             if j == 0:
-                ax1[i, j].set_ylabel(labels[nAx - 1 - i])
+                ax1[i, j].set_ylabel(labels[i + 1])
             
             if i == nAx - 2:
                 ax1[i, j].set_xlabel(labels[j])
@@ -60,19 +61,40 @@ def CornerPlot(data,labels):
 
 
 def SVMclassifier(data,classes):
+    print("Starting SVM classifier")
+    t0 = time.time();
     (m, n) = data.shape  # dimensionality and number of points, respectively
     
     # computationally cheaper way to compute Gram matrix
+    print("Computing Gram matrix...")
     gram = np.zeros((n, n))
     for i in range(n):
         for j in range(i + 1):
             gram[i, j] = np.dot(data[:, i], data[:, j])
             gram[j, i] = gram[i, j]
             
+    t1 = time.time()
+    print("Gram matrix complete. Time to complete: {0} ms".format(t1 - t0))
+    print("Training SVM...")
+            
     clf = svm.SVC(kernel='precomputed')
     clf.fit(gram, classes)
     
-    print(clf.score(gram, classes))
+    t2 = time.time()
+    print("SVM training complete. Training time for {0} points: {1} s"
+          .format(n, t2 - t1))
+    
+    print("Scoring...")
+    
+    score = clf.score(gram, classes)
+    
+    t3 = time.time()
+    
+    print("Scoring complete. Classification time for {0} points: {1} s"
+          .format(n, t3 - t2))
+    print("Classifier score: {0}".format(score))
+    
+    print("SVM classification complete. Total runtime: {0} s".format(t3 - t0))
 
 if __name__ == "__main__":
     # Import the data in 2 seperate stmts b/c genfromtxt doesnt like multityping
@@ -81,11 +103,12 @@ if __name__ == "__main__":
     subClass = np.genfromtxt('data.csv', delimiter=',',skip_header=2,usecols=5,
                              dtype=str)
     
-    colordata = np.array([u-g, g-r, r-i, i-z, u-r, u-i, u-z, g-i, g-z, r-z])
+    colordata = np.array([u-g, g-r, r-i, i-z]) #, u-r, u-i, u-z, g-i, g-z, r-z])
     
    
-    axLabels = ['$u-g$', '$g-r$', '$r-i$', '$i-z$', '$u-r$', '$u-i$', '$u-z$',
-                '$g-i$', '$g-z$', '$r-z$']
+    axLabels = ['$u-g$', '$g-r$', '$r-i$', '$i-z$'] #, '$u-r$', '$u-i$', '$u-z$',
+                #'$g-i$', '$g-z$', '$r-z$']
     
+    #CornerPlot(colordata,axLabels)
     SVMclassifier(colordata,subClass)
-    CornerPlot(colordata,axLabels)
+
