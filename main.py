c095aff7e9e65c422261e13ad849572560aa2ade
J. Andrew Casey-Clyde <jacaseyclyde@gmail.com>
2018-03-08 19:58:22 -0800

    added flag for svm scoring and data split in main

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from sklearn import svm
+from sklearn.model_selection import train_test_split
 
 def CornerPlot(data,labels):
     # convert string class labels to color labels (for use w/ scatter)
@@ -60,9 +61,9 @@ def CornerPlot(data,labels):
     fig1.savefig('./out/color_corner.png')
 
 
-def SVMclassifier(data,classes):
+def SVMclassifier(data,classes,score=False):
     print("Starting SVM classifier")
-    t0 = time.time();
+    t0 = time.time()
     (m, n) = data.shape  # dimensionality and number of points, respectively
     
     # computationally cheaper way to compute Gram matrix
@@ -84,17 +85,16 @@ def SVMclassifier(data,classes):
     print("SVM training complete. Training time for {0} points: {1} s"
           .format(n, t2 - t1))
     
-    print("Scoring...")
-    
-    score = clf.score(gram, classes)
-    
-    t3 = time.time()
-    
-    print("Scoring complete. Classification time for {0} points: {1} s"
-          .format(n, t3 - t2))
-    print("Classifier score: {0}".format(score))
-    
-    print("SVM classification complete. Total runtime: {0} s".format(t3 - t0))
+    if score == True:
+        print("Scoring...")
+        score = clf.score(gram, classes)
+        t2 = time.time()
+        print("Scoring complete. Classification time for {0} points: {1} s"
+              .format(n, t2 - t1))
+        print("Classifier score: {0}".format(score))
+        print("SVM classification complete. Total runtime: {0} s".format(t2 - t0))
+        
+    return clf
 
 if __name__ == "__main__":
     # Import the data in 2 seperate stmts b/c genfromtxt doesnt like multityping
@@ -103,12 +103,17 @@ if __name__ == "__main__":
     subClass = np.genfromtxt('data.csv', delimiter=',',skip_header=2,usecols=5,
                              dtype=str)
     
-    colordata = np.array([u-g, g-r, r-i, i-z]) #, u-r, u-i, u-z, g-i, g-z, r-z])
+    colordata = np.array([u-g, g-r, r-i, i-z]).T #, u-r, u-i, u-z, g-i, g-z, r-z])
     
    
     axLabels = ['$u-g$', '$g-r$', '$r-i$', '$i-z$'] #, '$u-r$', '$u-i$', '$u-z$',
                 #'$g-i$', '$g-z$', '$r-z$']
     
     #CornerPlot(colordata,axLabels)
-    SVMclassifier(colordata,subClass)
+    
+    # split data into training and test sets
+    clr_train, clr_test, cls_train, cls_test = train_test_split(colordata,
+                                                                subClass)
+    
+    #SVMclassifier(colordata,subClass)
 
