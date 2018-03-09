d28d7a4b45c97b7fd5c08f9ab0d4be32d4f154f1
J. Andrew Casey-Clyde <jacaseyclyde@gmail.com>
2018-03-08 22:39:43 -0800

    calculate ROC curves

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ import matplotlib.pyplot as plt
 from sklearn import svm
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import label_binarize
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.metrics import roc_curve, auc
 
 def CornerPlot(data,labels):
     # convert string class labels to color labels (for use w/ scatter)
@@ -119,14 +121,37 @@ def SVMAnalysis(X_train,X_test,y_train,y_test):
           .format(len(X_train), t_test))
     print("Classifier score: {0}".format(score))
     
+    # Generate analysis graphs
+    print("Generating ROC Curves...")
+    y_unique = np.unique(np.concatenate((y_train,y_test)))
+    
+    y_train = label_binarize(y_train, classes=y_unique)
+    y_test = label_binarize(y_test, classes=y_unique)
+    n_classes = len(y_unique)
+    
+    classifier = OneVsRestClassifier(svm.SVC(kernel='precomputed'))
+    y_score = classifier.fit(gram_train, y_train).decision_function(gram_test)
+    
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_score[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+        
+    fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), y_score.ravel())
+    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+    
     print("SVM analysis complete. Total runtime: {0} s".format(t2 - t0))
+    
+    return clf, fpr, tpr, roc_auc
     
 
 if __name__ == "__main__":
     # Import the data in 2 seperate stmts b/c genfromtxt doesnt like multityping
     u, g, r, i, z = np.genfromtxt('data.csv', delimiter=',', skip_header=2,
                                   usecols=(0,1,2,3,4)).T
-    subClass = np.genfromtxt('data.csv', delimiter=',',skip_header=2,usecols=5,
+    subclass = np.genfromtxt('data.csv', delimiter=',',skip_header=2,usecols=5,
                              dtype=str)
     
     colordata = np.array([u-g, g-r, r-i, i-z]).T #, u-r, u-i, u-z, g-i, g-z, r-z])
@@ -139,9 +164,9 @@ if __name__ == "__main__":
     
     # split data into training and test sets
     clr_train, clr_test, cls_train, cls_test = train_test_split(colordata,
-                                                                subClass,
+                                                                subclass,
                                                                 test_size=.5,
                                                                 random_state=0)
     
-    SVMAnalysis(clr_train, clr_test, cls_train, cls_test)
+    clf, fpr, tpr, roc_auc = SVMAnalysis(clr_train, clr_test, cls_train, cls_test)
 
