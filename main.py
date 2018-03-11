6d7d2d6b1900bc5b217e3161b68ccbd02215b3d2
J. Andrew Casey-Clyde <jacaseyclyde@gmail.com>
2018-03-10 19:04:24 -0800

    moved ROC curve generation to its own function

--- main.py
+++ main.py
@@ -83,66 +83,8 @@ def CornerPlot(data,cat,labels):
     fig1.savefig('./out/png/color_corner.png')
     
     print("Corner plots complete!")
-
-def GramMatrix(data):
-    (m, n) = data.shape  # dimensionality and number of points, respectively
     
-    # computationally cheaper way to compute Gram matrix
-    gram = np.zeros((n, n))
-    for i in range(n):
-        for j in range(i + 1):
-            gram[i, j] = np.dot(data[:, i], data[:, j])
-            gram[j, i] = gram[i, j]
-    
-    return gram
-
-def SVMAnalysis(X_train,X_test,y_train,y_test):
-    print("Starting Support Vector Machine analysis")
-    print("Initializing...")
-    t0 = time.time()
-    
-    clf = svm.SVC(kernel='precomputed')
-    
-    # Compute gram matrices for both sets
-    print("Computing training Gram matrix...")
-    t1 = time.time()
-    gram_train = GramMatrix(X_train.T)
-    t2 = time.time()
-    
-    t_gram_train = t2 - t1
-    print("Training Gram matrix complete. Time to compute for {0} points: {1} s"
-          .format(len(X_train),t_gram_train))
-    
-    print("Computing test Gram matrix...")
-    t1 = time.time()
-    gram_test = GramMatrix(X_test.T)
-    t2 = time.time()
-    
-    t_gram_test = t2 - t1
-    print("Test Gram matrix complete. Time to compute for {0} points: {1} s"
-          .format(len(X_test),t_gram_test))
-    
-    # Compute basic statistics for SVM
-    print("Training SVM...")
-    t1 = time.time()
-    clf.fit(gram_train, y_train)
-    t2 = time.time()
-    
-    t_train = t2 - t1
-    print("SVM training complete. Training time for {0} points: {1} s"
-          .format(len(X_train), t_train))
-    
-    print("Scoring...")
-    t1 = time.time()
-    score = clf.score(gram_test, y_test)
-    t2 = time.time()
-    
-    t_test = t2 - t1
-    print("Scoring complete. Classification time for {0} points: {1} s"
-          .format(len(X_train), t_test))
-    print("Classifier score: {0}".format(score))
-    
-    # Generate graphs/data for analysis
+def ROC(clfFn,X_train,X_test,y_train,y_test,clfType,shortType):
     print("Generating ROC Curves...")
     y_unique = np.unique(np.concatenate((y_train,y_test)))
     
@@ -150,107 +92,7 @@ def SVMAnalysis(X_train,X_test,y_train,y_test):
     y_test = label_binarize(y_test, classes=y_unique)
     n_classes = len(y_unique)
     
-    clf = OneVsRestClassifier(svm.SVC(kernel='precomputed'))
-    y_score = clf.fit(gram_train, y_train).decision_function(gram_test)
-    
-    fpr = dict()
-    tpr = dict()
-    roc_auc = dict()
-    for i in range(n_classes):
-        fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_score[:, i])
-        roc_auc[i] = auc(fpr[i], tpr[i])
-        
-    fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), y_score.ravel())
-    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
-    
-    # aggregate fpr
-    all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
-
-    # interpolate all ROC curves at this points
-    mean_tpr = np.zeros_like(all_fpr)
-    for i in range(n_classes):
-        mean_tpr += interp(all_fpr, fpr[i], tpr[i])
-
-    # average and compute AUC
-    mean_tpr /= n_classes
-
-    fpr["macro"] = all_fpr
-    tpr["macro"] = mean_tpr
-    roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
-    
-    fig1 = plt.figure(figsize=(12,12))
-    plt.plot(fpr['micro'], tpr['micro'], label='micro-average ROC curve (area = {0:0.2f})'
-             .format(roc_auc["micro"]), color='deeppink', linestyle=':',
-             linewidth=4)
-    
-    plt.plot(fpr['macro'], tpr['macro'], label='macro-average ROC curve (area = {0:0.2f})'
-             .format(roc_auc["macro"]), color='navy', linestyle=':', linewidth=4)
-    
-    colors = cycle([(0., 73./255., 73./255.), (255./255., 182./255., 119./255.),
-                    (73./255., 0./255., 146./255.), (182./255., 109./255., 255./255.),
-                    (109./255., 182./255., 255./255.), (182./255., 219./255., 255./255.),
-                    (146./255., 0., 0.), (146./255., 73./255., 0.),
-                    (36./255., 255./255., 36./255.), (255./255., 255./255., 109./255.),
-                    (0., 0., 0.)])
-    for i, color in zip(range(n_classes), colors):
-        plt.plot(fpr[i], tpr[i], color=color, lw=2, 
-                 label='Class {0} Stars (area = {1:0.2f})'
-                 .format(y_unique[i], roc_auc[i]))
-    
-    plt.plot([0, 1], [0, 1], 'k--', lw=2)
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('Stellar Class Receiver Operating Characteristics: Support Vector Machine')
-    plt.legend(loc="lower right")
-    
-    plt.show()
-    
-    fig1.savefig('./out/pdf/svm_roc.pdf')
-    fig1.savefig('./out/png/svm_roc.png')
-    
-    t2 = time.time()
-    print("SVM analysis complete. Total runtime: {0} s".format(t2 - t0))
-    
-    #return clf, fpr, tpr, roc_auc # can add this back in for debug/dev
-    
-def RandForestAnalysis(X_train,X_test,y_train,y_test):
-    print("Starting Random Forest analysis")
-    print("Initializing...")
-    t0 = time.time()
-    
-    clf = RandomForestClassifier(n_estimators=10000)
-    
-    # Compute basic statistics for SVM
-    print("Training Random Forest...")
-    t1 = time.time()
-    clf.fit(X_train, y_train)
-    t2 = time.time()
-    
-    t_train = t2 - t1
-    print("Random Forest training complete. Training time for {0} points: {1} s"
-          .format(len(X_train), t_train))
-    
-    print("Scoring...")
-    t1 = time.time()
-    score = clf.score(X_test, y_test)
-    t2 = time.time()
-    
-    t_test = t2 - t1
-    print("Scoring complete. Classification time for {0} points: {1} s"
-          .format(len(X_train), t_test))
-    print("Classifier score: {0}".format(score))
-    
-    # Generate graphs/data for analysis
-    print("Generating ROC Curves...")
-    y_unique = np.unique(np.concatenate((y_train,y_test)))
-    
-    y_train = label_binarize(y_train, classes=y_unique)
-    y_test = label_binarize(y_test, classes=y_unique)
-    n_classes = len(y_unique)
-    
-    clf = OneVsRestClassifier(RandomForestClassifier(n_estimators=10000))
+    clf = OneVsRestClassifier(clfFn)
     y_score = clf.fit(X_train, y_train).predict_proba(X_test)
     
     fpr = dict()
@@ -302,13 +144,109 @@ def RandForestAnalysis(X_train,X_test,y_train,y_test):
     plt.ylim([0.0, 1.05])
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
-    plt.title('Stellar Class Receiver Operating Characteristics: Random Forest')
+    plt.title('Stellar Class Receiver Operating Characteristics: {0}'.format(clfType))
     plt.legend(loc="lower right")
     
     plt.show()
     
-    fig1.savefig('./out/pdf/rf_roc.pdf')
-    fig1.savefig('./out/png/rf_roc.png')
+    fig1.savefig('./out/pdf/{0}_roc.pdf'.format(shortType))
+    fig1.savefig('./out/png/{0}_roc.png'.format(shortType))
+
+def GramMatrix(data):
+    (m, n) = data.shape  # dimensionality and number of points, respectively
+    
+    # computationally cheaper way to compute Gram matrix
+    gram = np.zeros((n, n))
+    for i in range(n):
+        for j in range(i + 1):
+            gram[i, j] = np.dot(data[:, i], data[:, j])
+            gram[j, i] = gram[i, j]
+    
+    return gram
+
+def SVMAnalysis(X_train,X_test,y_train,y_test):
+    print("Starting Support Vector Machine analysis")
+    print("Initializing...")
+    t0 = time.time()
+    
+    clf = svm.SVC(kernel='precomputed',probability=True)
+    
+    # Compute gram matrices for both sets
+    print("Computing training Gram matrix...")
+    t1 = time.time()
+    gram_train = GramMatrix(X_train.T)
+    t2 = time.time()
+    
+    t_gram_train = t2 - t1
+    print("Training Gram matrix complete. Time to compute for {0} points: {1} s"
+          .format(len(X_train),t_gram_train))
+    
+    print("Computing test Gram matrix...")
+    t1 = time.time()
+    gram_test = GramMatrix(X_test.T)
+    t2 = time.time()
+    
+    t_gram_test = t2 - t1
+    print("Test Gram matrix complete. Time to compute for {0} points: {1} s"
+          .format(len(X_test),t_gram_test))
+    
+    # Compute basic statistics for SVM
+    print("Training SVM...")
+    t1 = time.time()
+    clf.fit(gram_train, y_train)
+    t2 = time.time()
+    
+    t_train = t2 - t1
+    print("SVM training complete. Training time for {0} points: {1} s"
+          .format(len(X_train), t_train))
+    
+    print("Scoring...")
+    t1 = time.time()
+    score = clf.score(gram_test, y_test)
+    t2 = time.time()
+    
+    t_test = t2 - t1
+    print("Scoring complete. Classification time for {0} points: {1} s"
+          .format(len(X_train), t_test))
+    print("Classifier score: {0}".format(score))
+    
+    # Generate graphs/data for analysis
+    ROC(svm.SVC(kernel='precomputed',probability=True),gram_train,gram_test,y_train,y_test,"Support Vector Machine",'svm')
+    
+    t2 = time.time()
+    print("SVM analysis complete. Total runtime: {0} s".format(t2 - t0))
+    
+    #return clf, fpr, tpr, roc_auc # can add this back in for debug/dev
+    
+def RandForestAnalysis(X_train,X_test,y_train,y_test):
+    print("Starting Random Forest analysis")
+    print("Initializing...")
+    t0 = time.time()
+    
+    clf = RandomForestClassifier(n_estimators=100)
+    
+    # Compute basic statistics for SVM
+    print("Training Random Forest...")
+    t1 = time.time()
+    clf.fit(X_train, y_train)
+    t2 = time.time()
+    
+    t_train = t2 - t1
+    print("Random Forest training complete. Training time for {0} points: {1} s"
+          .format(len(X_train), t_train))
+    
+    print("Scoring...")
+    t1 = time.time()
+    score = clf.score(X_test, y_test)
+    t2 = time.time()
+    
+    t_test = t2 - t1
+    print("Scoring complete. Classification time for {0} points: {1} s"
+          .format(len(X_train), t_test))
+    print("Classifier score: {0}".format(score))
+    
+    # Generate graphs/data for analysis
+    ROC(RandomForestClassifier(n_estimators=100),X_train,X_test,y_train,y_test,"Random Forest",'rf')
     
     t2 = time.time()
     print("Random Forest analysis complete. Total runtime: {0} s".format(t2 - t0))
@@ -353,6 +291,6 @@ if __name__ == "__main__":
                                                                 test_size=.5, random_state=0)
     
     #clf, fpr, tpr, roc_auc = SVMAnalysis(clr_train, clr_test, cls_train, cls_test)
-    #SVMAnalysis(clr_train, clr_test, cls_train, cls_test)
-    #print("==================================================================")
+    SVMAnalysis(clr_train, clr_test, cls_train, cls_test)
+    print("==================================================================")
     RandForestAnalysis(clr_train, clr_test, cls_train, cls_test)
